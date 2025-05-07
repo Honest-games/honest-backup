c9a24bb0d9ca31e682d5e5bc035c589bc7f189cd
logo <logo@logo.com>
2025-05-07 20:10:48 +0300

    Init [deploy]

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e -x
+
 env > /app/src/.env
 
 # debug launch
@@ -7,7 +9,8 @@ env > /app/src/.env
 #sh ./pg_backup_and_send.sh
 
 echo "Starting pg backup on schedule $PG_BACKUP_SCHEDULE"
-crontab /app/src/crontab.txt
+sed "s|\${PG_BACKUP_SCHEDULE}|$PG_BACKUP_SCHEDULE|g" /app/src/crontab.txt > /app/src/crontab_resolved.txt
+crontab /app/src/crontab_resolved.txt
 cron
 touch /var/log/cron.log
 tail -f /var/log/cron.log
